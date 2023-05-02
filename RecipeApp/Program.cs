c4b1bed169cad7ad9f52847ed34c491b0ad2192d
Andeski <andexi42@gmail.com>
2023-05-02 12:15:20 +0300

    asd

--- RecipeApp/Program.cs
+++ RecipeApp/Program.cs
@@ -6,6 +6,7 @@ namespace RecipeApp
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
+        //Git + Visual studio ovat parhaat ohjelmistot ja toimivat aina :)
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
