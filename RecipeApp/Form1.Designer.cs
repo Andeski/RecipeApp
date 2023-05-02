3a783030aa0934ec79e282c2f30fc91aa4f51689
Andeski <andexi42@gmail.com>
2023-05-02 16:39:32 +0300

    List visual fixes, working details on item click, items converted to english

--- RecipeApp/Form1.Designer.cs
+++ RecipeApp/Form1.Designer.cs
@@ -28,91 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Center);
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
-            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
-            this.txtRecipeName = new System.Windows.Forms.TextBox();
-            this.btnAddRecipe = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
-            this.SuspendLayout();
+            ListViewItem listViewItem1 = new ListViewItem("");
+            panel1 = new Panel();
+            label1 = new Label();
+            listView1 = new ListView();
+            txtRecipeName = new TextBox();
+            btnAddRecipe = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel1.SuspendLayout();
+            SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(568, 78);
-            this.panel1.TabIndex = 0;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(568, 78);
+            panel1.TabIndex = 0;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(96, 18);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(150, 47);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Reseptit";
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(173, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(150, 47);
+            label1.TabIndex = 0;
+            label1.Text = "Reseptit";
+            label1.TextAlign = ContentAlignment.TopCenter;
             // 
             // listView1
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2});
-            listViewGroup1.Header = "ListViewGroup";
-            listViewGroup1.HeaderAlignment = System.Windows.Forms.HorizontalAlignment.Center;
-            listViewGroup1.Name = "listViewGroup1";
-            listViewGroup1.Subtitle = "asd";
-            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
-            listViewGroup1});
-            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1});
-            this.listView1.Location = new System.Drawing.Point(0, 84);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(344, 195);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
-
+            listView1.Cursor = Cursors.IBeam;
+            listView1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.Items.AddRange(new ListViewItem[] { listViewItem1 });
+            listView1.Location = new Point(12, 84);
+            listView1.Name = "listView1";
+            listView1.RightToLeft = RightToLeft.No;
+            listView1.Size = new Size(472, 295);
+            listView1.TabIndex = 1;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
             // 
             // txtRecipeName
             // 
-            this.txtRecipeName.Location = new System.Drawing.Point(12, 566);
-            this.txtRecipeName.Name = "txtRecipeName";
-            this.txtRecipeName.PlaceholderText = "Tee uusi resepti...";
-            this.txtRecipeName.Size = new System.Drawing.Size(239, 23);
-            this.txtRecipeName.TabIndex = 2;
+            txtRecipeName.Location = new Point(12, 566);
+            txtRecipeName.Name = "txtRecipeName";
+            txtRecipeName.PlaceholderText = "Find a recipe...";
+            txtRecipeName.Size = new Size(239, 23);
+            txtRecipeName.TabIndex = 2;
             // 
             // btnAddRecipe
             // 
-            this.btnAddRecipe.Location = new System.Drawing.Point(257, 566);
-            this.btnAddRecipe.Name = "btnAddRecipe";
-            this.btnAddRecipe.Size = new System.Drawing.Size(75, 23);
-            this.btnAddRecipe.TabIndex = 3;
-            this.btnAddRecipe.Text = "Find";
-            this.btnAddRecipe.UseVisualStyleBackColor = true;
+            btnAddRecipe.Location = new Point(257, 566);
+            btnAddRecipe.Name = "btnAddRecipe";
+            btnAddRecipe.Size = new Size(75, 23);
+            btnAddRecipe.TabIndex = 3;
+            btnAddRecipe.Text = "Find";
+            btnAddRecipe.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Location = new Point(12, 397);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(472, 152);
+            flowLayoutPanel1.TabIndex = 4;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(344, 601);
-            this.Controls.Add(this.btnAddRecipe);
-            this.Controls.Add(this.txtRecipeName);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.panel1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(496, 599);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(btnAddRecipe);
+            Controls.Add(txtRecipeName);
+            Controls.Add(listView1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -120,9 +121,8 @@
         private Panel panel1;
         private Label label1;
         private ListView listView1;
-        private ColumnHeader columnHeader1;
-        private ColumnHeader columnHeader2;
         private TextBox txtRecipeName;
         private Button btnAddRecipe;
+        private FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- RecipeApp/Form1.cs
+++ RecipeApp/Form1.cs
@@ -5,9 +5,14 @@ namespace RecipeApp
 
     public partial class Form1 : Form
     {
+
+        private List<Food> foods;
+
         public Form1()
         {
             InitializeComponent();
+            listView1.View = View.List;
+            listView1.MouseClick += listView1_MouseClick;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -29,16 +34,29 @@ namespace RecipeApp
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            //read .json
             string path = Path.Combine(Application.StartupPath, "recipes.json");
             string json = File.ReadAllText(path);
-            List<Food> foods = JsonConvert.DeserializeObject<List<Food>>(json);
+            foods = JsonConvert.DeserializeObject<List<Food>>(json);
+            ListViewItem item = null;
+            //clear any useless items
+            listView1.Items.Clear();
+
+            //set the column header text
+            listView1.Columns.Add("Food Name"); //Does not work, miksi?? Jos poistaa koko paska hajoo :D (1/3)
+
+            //set the auto-resize mode for columns
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent); //(2/3)
+            listView1.Columns[0].Width = -1; //(3/3)
+
 
             foreach (Food food in foods)
             {
-                // create a new listview item with the food name as the text
-                ListViewItem item = new ListViewItem(food.name);
 
-                // add sub-items for each ingredient and instruction
+                //create a new listview item with the food name as the text
+                item = new ListViewItem(food.name);
+
+                //add sub-items for each ingredient and instruction
                 foreach (string ingredient in food.ingredients)
                 {
                     item.SubItems.Add(new ListViewItem.ListViewSubItem(item, ingredient));
@@ -49,8 +67,73 @@ namespace RecipeApp
                     item.SubItems.Add(new ListViewItem.ListViewSubItem(item, instruction));
                 }
 
-                // add the item to the listview object
+                //add the item to the listview object
                 listView1.Items.Add(item);
+
+                //fixing the truncated/bunched up text with this //( huhhuh pitkä säätö:
+                int foodNameWidth = TextRenderer.MeasureText(item.Text, listView1.Font).Width;
+                int otherColumnsWidth = listView1.Columns.Cast<ColumnHeader>().Skip(1).Sum(c => c.Width);
+                int padding = listView1.ClientSize.Width - otherColumnsWidth - SystemInformation.VerticalScrollBarWidth;
+                listView1.Columns[0].Width = Math.Max(foodNameWidth + padding, listView1.Columns[0].Width);
+                //)
+            }
+
+            listView1.SelectedIndexChanged -= listView1_SelectedIndexChanged;
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0) //check if any items are selected
+            {
+                ListViewItem selected = listView1.SelectedItems[0];
+                string foodName = selected.Text;
+
+                //find the food item that matches the selected name
+                Food food = foods.FirstOrDefault(f => f.name == foodName);
+
+                //create a new form to display the food details
+                FoodDetailsForm detailsForm = new FoodDetailsForm(food);
+                detailsForm.Show();
+            }
+        }
+
+        public partial class FoodDetailsForm : Form
+        {
+            public FoodDetailsForm(Food food)
+            {
+                //set title of window to the name of the selected food
+                Text = food.name;
+
+                //create a table layout panel to hold the controls
+                TableLayoutPanel table = new TableLayoutPanel();
+                table.Dock = DockStyle.Fill;
+                table.AutoSize = true;
+                table.AutoScroll = true;
+                table.ColumnCount = 1;
+                table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100));
+
+                //display ingredients
+                string ingredients = string.Join(Environment.NewLine, food.ingredients);
+                Label ingredientsLabel = new Label
+                {
+                    Text = $"Ingredients:{Environment.NewLine}{ingredients}",
+                    Dock = DockStyle.Fill,
+                    AutoSize = true
+                };
+                table.Controls.Add(ingredientsLabel, 0, 0);
+
+                //display instructions
+                string instructions = string.Join(Environment.NewLine, food.instructions);
+                Label instructionsLabel = new Label
+                {
+                    Text = $"Instructions:{Environment.NewLine}{instructions}",
+                    Dock = DockStyle.Fill,
+                    AutoSize = true
+                };
+                table.Controls.Add(instructionsLabel, 0, 1);
+
+                //add table layout panel to the form
+                Controls.Add(table);
             }
         }
     }
